5a3539e0718a05f321d98a8e0db52e4cd67989a9
Michel <>
2024-06-09 00:21:30 +0200

    tests for filter function done

--- cmd/filter.go
+++ cmd/filter.go
@@ -8,11 +8,14 @@ func filterComments(posts []Post, filterParameter string) []Post {
 	if filterParameter == "" {
 		return posts
 	}
+
+	normalizedFilterParameter := strings.ReplaceAll(filterParameter, "\n", " ")
+
 	for i, post := range posts {
 		var filteredComments []Comment
 		for _, comment := range post.Comments {
 			normalizedBody := strings.ReplaceAll(comment.Body, "\n", " ") //need to replace the \n with blank space in the body if you search it
-			if strings.Contains(normalizedBody, filterParameter) {
+			if strings.Contains(normalizedBody, normalizedFilterParameter) {
 				filteredComments = append(filteredComments, comment)
 			}
 		}
--- cmd/filter_test.go
+++ cmd/filter_test.go
@@ -1,7 +1,130 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestFilterComments(t *testing.T) {
-	
+func TestFilterCommentsValid(t *testing.T) {
+	mockPosts := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 1, Name: "Test Comment 1", Email: "test1@example.com", Body: "this Should appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this Shouldnt appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this is a comment with\nin it"},
+			},
+		},
+	}
+
+	expectedPost := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 1, Name: "Test Comment 1", Email: "test1@example.com", Body: "this Should appear in the Filter"},
+			},
+		},
+	}
+	filteredPosts := filterComments(mockPosts, "this Should appear in the Filter")
+
+	if !reflect.DeepEqual(filteredPosts, expectedPost) {
+		t.Errorf("filterComments returned unexpected results: got %v want %v", filteredPosts, expectedPost)
+	}
+
+}
+
+// TODO Maybe test input with \n ?
+func TestFilterCommentsEdgeCase(t *testing.T) {
+	mockPosts := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 1, Name: "Test Comment 1", Email: "test1@example.com", Body: "this Should appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this Shouldnt appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this is a comment with\nin it"},
+			},
+		},
+	}
+
+	expectedPost := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this is a comment with\nin it"},
+			},
+		},
+	}
+
+	filteredPosts := filterComments(mockPosts, "this is a comment with in it")
+
+	if !reflect.DeepEqual(filteredPosts, expectedPost) {
+		t.Errorf("filterComments returned unexpected results: got %v want %v", filteredPosts, expectedPost)
+	}
+
+}
+
+func TestFilterCommentsEmptyInput(t *testing.T) {
+	mockPosts := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 1, Name: "Test Comment 1", Email: "test1@example.com", Body: "this Should appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this Shouldnt appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this is a comment with\nin it"},
+			},
+		},
+	}
+
+	unfliteredPosts := filterComments(mockPosts, "")
+
+	if !reflect.DeepEqual(unfliteredPosts, mockPosts) {
+		t.Errorf("filterComments returned unexpected results: got %v want %v", unfliteredPosts, mockPosts)
+	}
+
+}
+
+func TestFilterCommentsNonexistentInput(t *testing.T) {
+	mockPosts := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+			Comments: []Comment{
+				{PostID: 1, ID: 1, Name: "Test Comment 1", Email: "test1@example.com", Body: "this Should appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this Shouldnt appear in the Filter"},
+				{PostID: 1, ID: 2, Name: "Test Comment 2", Email: "test2@example.com", Body: "this is a comment with\nin it"},
+			},
+		},
+	}
+
+	expectedPost := []Post{
+		{
+			UserID: 1,
+			ID:     1,
+			Title:  "Test Post",
+			Body:   "Body of test post",
+		},
+	}
+
+	filteredPosts := filterComments(mockPosts, "this text is not included in any comment")
+
+	if !reflect.DeepEqual(filteredPosts, expectedPost) {
+		t.Errorf("filterComments returned unexpected results: got %v want %v", filteredPosts, expectedPost)
+	}
 }
